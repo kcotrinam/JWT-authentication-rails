be6112a0603e30ba5ea01ed51fb4eb22eec3dfa9
Kevin Cotrina <kcotrinma92@gmail.com>
2021-04-26 09:08:56 -0500

    Abstract decode token

--- app/controllers/baloons_controller.rb
+++ app/controllers/baloons_controller.rb
@@ -1,27 +1,18 @@
 class BaloonsController < ApplicationController
+  before_action :authenticate, only: [:create]
+
   def index
     baloons = Baloon.all
-    byebug
     render json: { ballons: baloons }
   end
 
   def create
-    authorization_header = request.headers[:Authorization]
-    if !authorization_header
-      render status: :unauthorized
-    else
-      token = authorization_header.split(' ')[1]
-      secret_key = Rails.application.secrets.secret_key_base[0]
-      decoded_token = JWT.decode(token, secret_key)
+    baloon = Baloon.create(
+      name: params[:name],
+      quantity: params[:quantity],
+      user: @user
+    )
 
-      user = User.find(decoded_token[0]["user_id"])
-
-      baloon = Baloon.create(
-        name: params[:name],
-        quantity: params[:quantity]
-      )
-
-      render json: { baloon: baloon }
-    end
+    render json: { baloon: baloon }
   end
 end

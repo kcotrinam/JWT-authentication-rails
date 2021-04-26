a08453677794af0f8aa597aba171e11e7635bc51
Kevin Cotrina <kcotrinma92@gmail.com>
2021-04-25 19:57:28 -0500

    Add Create user actions controller. Add routes to create user. Return json as response.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   
-  def method_name
+  def create
     # Read in users params
     # Create a new user with them
+    user = User.create(
+      name: params[:name],
+      username: params[:username]
+      email: params[:email]
+      password: params[:password]
+    )
+
     # Send back the created user
+    render json: user
   end
   
 end
